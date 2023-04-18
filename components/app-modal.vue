6c84a9c32ad4fa47696faf861f7f3bd5e4802912
David Arenas <davejs136@gmail.com>
2023-04-18 10:07:53 -0400

    feat: add app-modal component

---
+++ components/app-modal.vue
@@ -0,0 +1,18 @@
+<template>
+  <GDialog v-model="active" max-width="900">
+    <slot></slot>
+  </GDialog>
+</template>
+
+<script lang="ts" setup>
+import { GDialog } from "gitart-vue-dialog";
+import "gitart-vue-dialog/dist/style.css";
+
+const emit = defineEmits<{ (e: "update:modelValue", value: boolean): void }>();
+const props = defineProps<{ modelValue: boolean }>();
+
+const active = computed({
+  get: () => props.modelValue,
+  set: (val) => emit("update:modelValue", val),
+});
+</script>
